739750d773eb31479e2a3a67894a04587ef07036
kijv <93486795+kijv@users.noreply.github.com>
2026-02-13 17:14:03 -0500

    fix token_url

--- crates/oas3/src/spec/flows.rs
+++ crates/oas3/src/spec/flows.rs
@@ -151,7 +151,7 @@ pub struct AuthorizationCodeFlow {
     /// The token URL to be used for this flow.
     ///
     /// This MUST be in the form of a URL. The OAuth2 standard requires the use of TLS.
-    pub token_url: Url,
+    pub token_url: UrlWithRelativeUrlAsFileUrl,
 
     /// The URL to be used for obtaining refresh tokens.
     ///
